fa41c14d513a417c9359a174ed753dace33750c7
Celtia <celtiacastelo@gmail.com>
2026-04-18 19:29:35 +0200

    arreglado paginacion y incluidas search bars

--- universityregister/catalog/views.py
+++ universityregister/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 # Create your views here.
 
@@ -34,20 +35,56 @@ from django.views import generic
 class CourseListView(generic.ListView):
     model = Course
     paginate_by = 10
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('search')
+        
+        if query:
+            # Filtra si el título O el código del curso contienen la palabra
+            queryset = queryset.filter(
+                Q(title__icontains=query) | Q(course_code__icontains=query)
+            )
+            
+        return queryset
 
 class CourseDetailView(generic.DetailView):
     model = Course
 
 class ProfesorListView(generic.ListView):
     model = Profesor
-    paginate_by = 12
+    paginate_by = 2
+    def get_queryset(self):
+        # Obtenemos el queryset original
+        queryset = super().get_queryset()
+        
+        # Buscamos si hay un parámetro 'search' en la URL
+        query = self.request.GET.get('search')
+        
+        if query:
+            # Filtramos por el campo 'name' (ajusta si el campo se llama distinto)
+            queryset = queryset.filter(name__icontains=query)
+            
+        return queryset
 
 class ProfesorDetailView(generic.DetailView):
     model = Profesor
+    #for adding search functionality
+    
 
 class StudentListView(generic.ListView):
     model = Student
     paginate_by = 3
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('search')
+        
+        if query:
+            # filters students by first name or last name containing the search query. Q objects allow for complex queries with OR conditions.
+            queryset = queryset.filter(
+                Q(first_name__icontains=query) | Q(last_name__icontains=query)
+            )
+            
+        return queryset
 
 class StudentDetailView(generic.DetailView):
     model = Student
